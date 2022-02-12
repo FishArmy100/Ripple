f2333530626c7f53fed763bea89d43dddafc2b07
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2022-02-11 22:02:21 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ripple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
