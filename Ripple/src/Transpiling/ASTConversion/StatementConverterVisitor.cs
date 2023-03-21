54843082912a5de97b62289f78f69ae5c312ba50
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-03-20 20:15:29 -0400

    Bump

--- Ripple/src/Transpiling/ASTConversion/StatementConverterVisitor.cs
+++ Ripple/src/Transpiling/ASTConversion/StatementConverterVisitor.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ripple.Validation.Info.Statements;
+using Ripple.Validation.Info.Expressions;
 using Ripple.Transpiling.C_AST;
 using Ripple.Utils;
+using Ripple.Utils.Extensions;
 
 namespace Ripple.Transpiling.ASTConversion
 {
@@ -14,6 +16,8 @@ namespace Ripple.Transpiling.ASTConversion
         private readonly ExpressionConverterVisitor m_ExpressionConverter;
         private readonly TypeConverterVisitor m_TypeConverter;
         private readonly List<CIncludeStmt> m_Includes;
+
+        private Stack<int> m_VariableCount = new Stack<int>();
 
         public StatementConverterVisitor(CArrayRegistry registry, List<CIncludeStmt> includes)
         {
@@ -24,7 +28,10 @@ namespace Ripple.Transpiling.ASTConversion
 
         public List<CStatement> VisitTypedBlockStmt(TypedBlockStmt typedBlockStmt)
         {
-            return ToList(new CBlockStmt(typedBlockStmt.Accept(this)));
+            m_VariableCount.Push(0);
+            var returned = ToList(new CBlockStmt(typedBlockStmt.Accept(this)));
+            m_VariableCount.Pop();
+            return returned;
         }
 
         public List<CStatement> VisitTypedBreakStmt(TypedBreakStmt typedBreakStmt)
@@ -39,6 +46,7 @@ namespace Ripple.Transpiling.ASTConversion
 
         public List<CStatement> VisitTypedExprStmt(TypedExprStmt typedExprStmt)
         {
+            m_ExpressionConverter.VariableSufix = $"var_{m_VariableCount.Peek()}";
             ExpressionConversionResult result = typedExprStmt.Expression.Accept(m_ExpressionConverter);
             List<CStatement> statements = result.GeneratedVariables
                 .Cast<CStatement>()
@@ -55,17 +63,41 @@ namespace Ripple.Transpiling.ASTConversion
 
         public List<CStatement> VisitTypedFileStmt(TypedFileStmt typedFileStmt)
         {
+            m_VariableCount.Push(0);
             var statements = typedFileStmt.Statements
                 .Select(s => s.Accept(this))
                 .SelectMany(s => s)
                 .ToList();
+
+            m_VariableCount.Pop();
 
             return ToList(new CFileStmt(m_Includes, statements, typedFileStmt.FilePath, CFileType.Source));
         }
 
         public List<CStatement> VisitTypedForStmt(TypedForStmt typedForStmt)
         {
-            throw new NotImplementedException();
+            m_VariableCount.Push(0);
+
+            Option<Pair<CVarDecl, List<CStatement>>> initalizerResult = typedForStmt.Initalizer.Match(
+                ok =>
+                {
+                    List<CStatement> statements = ok.Accept(this);
+                    CVarDecl var = (CVarDecl)statements[statements.Count - 1];
+                    statements.RemoveAt(statements.Count - 1);
+                    return new Pair<CVarDecl, List<CStatement>>(var, statements);
+                }, 
+                () => new Option<Pair<CVarDecl, List<CStatement>>>());
+
+            List<CStatement> extraStatements = initalizerResult.Match(ok => ok.Second, () => new List<CStatement>());
+
+            Option<Pair<CExpression, List<CStatement>>> conditionResult = typedForStmt.Condition.Match(
+                ok =>
+                {
+
+                    var result = ok.Accept(m_ExpressionConverter);
+                    return new Pair<CExpression, List<CStatement>>(result.Expression, result.GeneratedVariables.Cast<CStatement>().ToList());
+                },
+                () => new Option<Pair<CExpression, List<CStatement>>>());
         }
 
         public List<CStatement> VisitTypedFuncDecl(TypedFuncDecl typedFuncDecl)
@@ -107,5 +139,19 @@ namespace Ripple.Transpiling.ASTConversion
         {
             return new List<CStatement>(statements);
         }
+
+        private ExpressionConversionResult VisitExpression(TypedExpression expression)
+		{
+            ExpressionConversionResult result = expression.Accept(m_ExpressionConverter);
+            int count = m_VariableCount.Pop();
+            m_VariableCount.Push(++count);
+            m_ExpressionConverter.VariableSufix = GetPostfix();
+            return result;
+		}
+
+        private string GetPostfix()
+		{
+            return m_VariableCount.Select(c => c.ToString()).Concat("_");
+		}
     }
 }
